e62201c6f8e3370999cf51c20cbde28ec346144a
Aditya Salapaka <salapaka.aditya@gmail.com>
2015-05-01 17:13:21 +0530

    added script, did exploratory analysis

---
+++ regmodels.R
@@ -0,0 +1,18 @@
+data(mtcars)
+mtcars$cyl <- factor(mtcars$cyl)
+mtcars$vs <- factor(mtcars$vs)
+mtcars$am <- factor(mtcars$am, labels = c("Automatic", "Manual"))
+mtcars$gear <- factor(mtcars$gear)
+mtcars$carb <- factor(mtcars$carb)
+str(mtcars)
+
+#pairs(mtcars)
+library("GGally")
+ggpairs(mtcars, axisLabels = "internal")
+
+library("ggplot2")
+
+g <- ggplot(mtcars, aes(am, mpg))
+g <- g + geom_boxplot() + xlab("Transmission Type") + ylab("MPG") + 
+        geom_jitter()
+print(g)
